04c08321bb3bbff6106e3db7d4de7ce82e7787f3
Mohamed Wattas <medwattass@gmail.com>
2023-06-09 17:48:05 +0100

    first commit

---
+++ login_register_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.root),
+    path('login', views.login_page),
+    path('register', views.registration_page),
+    path('dashboard', views.dashboard),
+    path('registring', views.registration),
+    path('loging_in', views.loging_in),
+    path('logout', views.logout)
+]
